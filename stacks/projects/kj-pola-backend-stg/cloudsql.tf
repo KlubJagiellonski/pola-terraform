10e90a2ea3d3c809e784568d062ec6e5e8fa7190
skibinstein <blank>
2026-01-22 22:19:09 +0100

    Doddanie sql oraz checkov

---
+++ stacks/projects/kj-pola-backend-stg/cloudsql.tf
@@ -0,0 +1,61 @@
+module "cloudsql" {
+  source  = "terraform-google-modules/sql-db/google//modules/postgresql"
+  version = "~> 27.1"
+
+  project_id = var.project_id
+  region     = var.region
+  name       = "${var.name.app}-${var.name.component}-${var.name.env}-db"
+
+  database_version = "POSTGRES_16"
+  tier             = var.cloudsql_tier
+
+  deletion_protection = true
+
+  disk_type             = var.cloudsql_disk_type
+  disk_size             = var.cloudsql_disk_size
+  disk_autoresize       = var.cloudsql_disk_autoresize
+  disk_autoresize_limit = var.cloudsql_disk_autoresize_limit
+
+  database_flags = [
+    {
+      name  = "autovacuum"
+      value = "on"
+    },
+    {
+      name  = "timezone"
+      value = "Europe/Warsaw"
+    }
+  ]
+
+  insights_config = var.cloudsql_insights_enabled ? {
+    query_insights_enabled  = true
+    query_string_length     = 1024
+    record_application_tags = false
+    record_client_address   = false
+  } : null
+
+  backup_configuration = var.cloudsql_backup_enabled ? {
+    enabled                        = true
+    start_time                     = var.cloudsql_backup_start_time
+    point_in_time_recovery_enabled = true
+    transaction_log_retention_days = 7
+    backup_retention_settings = {
+      retained_backups = var.cloudsql_backup_retained_backups
+      retention_unit   = "COUNT"
+    }
+  } : null
+
+  ip_configuration = {
+    ipv4_enabled                                  = false
+    private_network                               = module.vpc.network.self_link
+    enable_private_path_for_google_cloud_services = true
+  }
+
+  maintenance_window_day          = var.cloudsql_maintenance_window_day
+  maintenance_window_hour         = var.cloudsql_maintenance_window_hour
+  maintenance_window_update_track = var.cloudsql_maintenance_window_update_track
+
+  depends_on = [
+    module.vpc
+  ]
+}
--- stacks/projects/kj-pola-backend-stg/terraform.tfvars
+++ stacks/projects/kj-pola-backend-stg/terraform.tfvars
@@ -45,6 +45,20 @@ workload_identity_provider_id = "github-actions-provider"
 terraform_runner_sa_name      = "terraform-runner"
 
 # VPC Networking Configuration
-vpc_subnet_cidr                = "10.0.0.0/24"
-vpc_subnet_description         = "Subnet dla Cloud Run Direct VPC Egress i Cloud SQL"
-private_service_access_cidr    = "10.1.0.0/24"
+vpc_subnet_cidr             = "10.0.0.0/24"
+vpc_subnet_description      = "Subnet dla Cloud Run Direct VPC Egress i Cloud SQL"
+private_service_access_cidr = "10.1.0.0/24"
+
+# Cloud SQL Configuration
+cloudsql_tier                            = "db-f1-micro"
+cloudsql_disk_type                       = "PD_SSD"
+cloudsql_disk_size                       = 20
+cloudsql_disk_autoresize                 = true
+cloudsql_disk_autoresize_limit           = 60
+cloudsql_insights_enabled                = true
+cloudsql_backup_enabled                  = true
+cloudsql_backup_start_time               = "02:00"
+cloudsql_backup_retained_backups         = 30
+cloudsql_maintenance_window_day          = 1
+cloudsql_maintenance_window_hour         = 1
+cloudsql_maintenance_window_update_track = "stable"
